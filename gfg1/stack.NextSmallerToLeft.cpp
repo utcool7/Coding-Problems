f3ab02f01308bed15f4ddbe846653321e9b7b303
utcool7 <31389321+utcool7@users.noreply.github.com>
2020-04-13 19:03:12 +0530

    Add files via upload

---
+++ gfg1/stack.NextSmallerToLeft.cpp
@@ -0,0 +1,63 @@
+//time :O(n)
+//space :O(n)
+#include <bits/stdc++.h>
+using namespace std;
+
+vector <long long int> nextLargest(vector <long long int > &arr ,long long int  n)
+{
+    vector <long long int> v;
+    stack <long long int> s;
+    for(int i =0 ;i<n ;i++)
+    {
+        if(s.size() == 0)
+            v.push_back(-1);
+        else if(s.size()>0 && s.top()<arr[i])
+        {
+            v.push_back(s.top());
+        }
+        else if(s.size()>0 && s.top()>=arr[i])
+        {
+            while(s.size()>0 && s.top()>=arr[i])
+            {
+                s.pop();
+                
+            }
+            if(s.size() == 0)
+            {
+                v.push_back(-1);
+            }
+            else
+            {
+                v.push_back(s.top());
+            }
+        }
+        s.push(arr[i]);
+            
+    }
+    
+    return v ;
+}
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    long long int n ;
+	    cin>>n;
+	    vector <long long int> arr(n);
+	    for(int i = 0 ;i<n;i++)
+	    {
+	        cin>>arr[i];
+	    }
+	    vector <long long int> res = nextLargest(arr,n);
+	    for(auto x:res)
+	    {
+	        cout<<x<<" ";
+	    }
+	    cout<<endl;
+	    
+	}
+	return 0;
+}
+
