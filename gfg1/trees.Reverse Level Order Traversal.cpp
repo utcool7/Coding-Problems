b64866a389598f2375eef2e505cc98f51173d200
utcool7 <31389321+utcool7@users.noreply.github.com>
2020-05-01 19:09:46 +0530

    Add files via upload

---
+++ gfg1/trees.Reverse Level Order Traversal.cpp
@@ -0,0 +1,42 @@
+//https://practice.geeksforgeeks.org/problems/reverse-level-order-traversal/1
+
+//time :O(n^2)
+//space :O(n)
+
+/* A tree Node has data, pointer to left child
+   and a pointer to right child  
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+}; */
+
+int height(Node *root)
+{
+    if(root == NULL)
+        return 0;
+    return max(height(root->left),height(root->right)) + 1;        
+}
+
+void level(Node *root, int k)
+{
+    if(root == NULL)
+        return ;
+    if(k == 0)
+    {
+        cout<<root->data <<" ";
+        return ;
+    }
+    level(root->left, k - 1);
+    level(root->right,k - 1);
+}
+void reversePrint(Node *root)
+{
+    //Your code here
+    int h = height(root);
+    for(int i= h ; i >=0 ; i--)
+    {
+        level(root,i);
+    }
+}
