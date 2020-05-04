ac0c1533bc87a70e835f14764cd771d9f0560b1b
utcool7 <31389321+utcool7@users.noreply.github.com>
2020-05-04 18:29:44 +0530

    Add files via upload

---
+++ gfg1/trees.Mirror Tree.cpp
@@ -0,0 +1,18 @@
+//https://practice.geeksforgeeks.org/problems/mirror-tree/1
+
+// time : O(n)
+//space : O(n)
+
+/* Should convert tree to its mirror */
+void mirror(Node* node) 
+{
+     // Your Code Here
+     if(node == NULL)
+         return ;
+     auto temp = node->left;
+     node->left = node->right;
+     node->right = temp ;
+     mirror( node -> left );
+     mirror( node -> right );
+    
+}
